bac3038d97dd643f0d41105a07f5e7c0c00406bf
Luiz Saluti <luizsaluti@Luizs-iMac.local>
2019-08-26 21:53:38 -0300

    Usando funções puras para criar um componente simples

--- pure-functions/src/title.js
+++ pure-functions/src/title.js
@@ -2,24 +2,33 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-    getDefaultProps: function(){
-        return {
-            nome:"Desconhecido",
-            sobreNome: "sem Sobrenome",
-            informacoesPessoais:{
-                nascimento: "01/01/1970"
-            }
-        }
-    },
-    render:function(){
-        return (
-            <div>
-                <h1>Ola {this.props.nome} {this.props.sobreNome}!</h1>
-                <h3>Nascido a {this.props.informacoesPessoais.nascimento}</h3>
-            </div>
-        )
-    }
-})
+const Title = ({nome, sobreNome}) =>  (
+    <h1>Olá {`${nome} ${sobreNome}`}</h1> 
+)//usando arrow functions. Omitindo chaves e a palavra-chave return
+ //na string está sendo aplicado o conceito de "Template String"
+
+Title.defaultProps = {
+    nome: 'Desconhecido',
+    sobreNome: 'Sem Sobrenome'
+}
+// const Title = React.createClass({
+//     getDefaultProps: function(){
+//         return {
+//             nome:"Desconhecido",
+//             sobreNome: "sem Sobrenome",
+//             informacoesPessoais:{
+//                 nascimento: "01/01/1970"
+//             }
+//         }
+//     },
+//     render:function(){
+//         return (
+//             <div>
+//                 <h1>Ola {this.props.nome} {this.props.sobreNome}!</h1>
+//                 <h3>Nascido a {this.props.informacoesPessoais.nascimento}</h3>
+//             </div>
+//         )
+//     }
+// })
 
 export default Title
