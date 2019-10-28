4c3c160aabb1f2cbbc086fa2f1fd0829f460244f
Luiz Saluti <luizsaluti@Luizs-iMac.local>
2019-10-28 20:01:15 -0300

    Segunda etapa da componentização, hierarquizando componentes

--- composicao/src/app.js
+++ composicao/src/app.js
@@ -3,13 +3,14 @@
 import React,{ Component } from 'react'
 import Title from './title'
 import Square from './square'
-import Button from './button'
+import LikeButton from './like-button'
 
 class App extends Component{
     render(){
         return (
             <div className="conteiner" >
-               <Button>Teste</Button>
+               {/* <Button>Teste</Button> */}
+               <LikeButton></LikeButton>
             </div>
         )
     }
